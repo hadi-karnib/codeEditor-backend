d2998ca46c795682c9dc352a656475e949cfb72f
karniib <hadikarnib03@gmail.com>
2024-07-28 20:31:39 +0300

    added seeders to db

--- codeEditor_backend/database/seeders/DatabaseSeeder.php
+++ codeEditor_backend/database/seeders/DatabaseSeeder.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        $this->call([
+            UserSeeder::class,
+            code_fileSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([

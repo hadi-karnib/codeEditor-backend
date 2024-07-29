81be75168fac894501f0746465cc5885e0d558f2
karniib <hadikarnib03@gmail.com>
2024-07-29 17:34:42 +0300

    fixed user model

--- codeEditor_backend/app/Models/User.php
+++ codeEditor_backend/app/Models/User.php
@@ -55,6 +55,8 @@ class User extends Authenticatable implements JWTSubject
     }
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'role' => $this->role,
+        ];
     }
 }

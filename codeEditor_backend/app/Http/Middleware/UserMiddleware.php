4e2b4cf937306ce6931d49d2912ecfb2103e255e
karniib <hadikarnib03@gmail.com>
2024-07-29 17:34:28 +0300

    added user middleware

---
+++ codeEditor_backend/app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        } else {
+            if (Auth::user()->role !== 'admin' && Auth::user()->role !== 'user') {
+                return response()->json(['error' => 'Unauthorized'], 403);
+            }
+            return $next($request);
+        }
+    }
+}
